fa230d36796deb38441b5b78b6ab5a26fc435901
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-24 17:47:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
